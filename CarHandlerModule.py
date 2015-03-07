68d46ce77d4474ac4a4fd64e78585087ba98c4f0
Sebastian <sebastian.wahrmund@googlemail.com>
2015-03-07 12:29:55 +0100

    Updates Car & CarHandler

--- CarHandlerModule.py
+++ CarHandlerModule.py
@@ -6,6 +6,7 @@
 
 # package include(s)
 from CarModule import Car
+from Location import Location
 
 
 ################################################################################
@@ -16,8 +17,29 @@ class CarHandler(object):
         self.__isInit = False
     
     #_______________________________________ 
+    def __getitem__(self, idx):
+        return self.__listOfCars[idx]
+    
+    #_______________________________________ 
+    def __repr__(self):
+        return self.__listOfCars.__repr__()
+    
+    #_______________________________________ 
+    def __str__(self):
+        return self.__listOfCars.__repr__()
+    
+    #_______________________________________ 
     def receiveSignal(self, sigFunc):
         pass
+    
+    ########################################        
+    # Getter
+    ######################################## 
+
+    #_______________________________________ 
+    def getCarList(self):
+        return self.__listOfCars
+
 
     ########################################        
     # Initialisation
@@ -32,10 +54,22 @@ class CarHandler(object):
     def isInit(self): return self.__isInit
     
     #_______________________________________ 
-    def initialise(self):
-        pass
-    
+    def initialise(self, locationList):
+        try:
+            self.clear()
+            for location in locationList:
+                self.__listOfCars.append(Car(location))
+       
+            self.__isInit = True
+        
+        except:
+            print "CarHandler - Error during initialisation"
+            self.clear()
+            raise
+
     #_______________________________________ 
+    
+    
     #_______________________________________ 
     #_______________________________________ 
     
--- CarModule.py
+++ CarModule.py
@@ -5,16 +5,25 @@
 # python include(s)
 
 # package include(s)
-from Location import Location
 
 
 ################################################################################
 class Car(object):
     #_______________________________________ 
-    def __init__(self):
-        self.__location = Location()
-        self.__intensity = None
+    def __init__(self, location, intensity = 0):
+        self.__location = location
+        self.__intensity = intensity
 
+    #_______________________________________ 
+    def __str__(self):
+        return "Car({0},{1},{2})".format(self.__location.x,
+                                         self.__location.y,
+                                         self.__intensity)
+    
+    #_______________________________________ 
+    def __repr__(self):
+        return self.__str__()
+    
     #_______________________________________ 
     def getLocation(self): return self.__location
     def getIntensity(self): return self.__intensity
@@ -27,6 +36,4 @@ class Car(object):
     def updateLocation(self, location):
         self.__location = location
     
-    #_______________________________________ 
-
 

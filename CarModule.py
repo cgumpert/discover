b0f95b9d77906f83f3419eac27ea3fcecf879a60
Sebastian <sebastian.wahrmund@googlemail.com>
2015-03-07 12:06:10 +0100

    Add CarHandler, update car

--- CarModule.py
+++ CarModule.py
@@ -1,2 +1,32 @@
+################################################################################
+# Implementation of Car Module
+################################################################################
+
+# python include(s)
+
+# package include(s)
+from location import location
+
+
+################################################################################
 class Car(object):
-    pass
+    #_______________________________________ 
+    def __init__(self):
+        self.__location = location(0,0)
+        self.__intensity = None
+
+    #_______________________________________ 
+    def getLocation(self): return self.__location
+    def getIntensity(self): return self.__intensity
+
+    #_______________________________________ 
+    def receiveSignal(self, intensity):
+        self.__intensity = intensity
+
+    #_______________________________________ 
+    def updateLocation(self, location):
+        self.__location = location
+    
+    #_______________________________________ 
+
+
